817b37fd5fe049e341f70c51a2fa148951206061
RemcoKuipers <remslaap@live.nl>
2026-04-07 14:01:55 +0200

    Login en registatie paginas werkend gemaakt met behulp van de novi api

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,19 +1,83 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import "./Register.css";
 
 function Register() {
+    const { register } = useAuth();
     const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    function handleRegister() {
-        navigate("/login");
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        setErrorMessage("");
+        setSuccessMessage("");
+
+        const success = await register(
+            email,
+            password
+        );
+
+        if (success) {
+            setSuccessMessage(
+                "Account registered successfully."
+            );
+
+            setTimeout(() => {
+                navigate("/login");
+            }, 1500);
+        } else {
+            setErrorMessage(
+                "Registration failed."
+            );
+        }
     }
 
     return (
-        <section>
+        <section className="auth-page">
             <h1>Register</h1>
 
-            <button onClick={handleRegister}>
-                Register
-            </button>
+            <form
+                className="auth-form"
+                onSubmit={handleRegister}
+            >
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) =>
+                        setEmail(e.target.value)
+                    }
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) =>
+                        setPassword(e.target.value)
+                    }
+                />
+
+                <button type="submit">
+                    Register
+                </button>
+                {errorMessage && (
+                    <p className="form-message error">
+                        {errorMessage}
+                    </p>
+                )}
+
+                {successMessage && (
+                    <p className="form-message success">
+                        {successMessage}
+                    </p>
+                )}
+            </form>
         </section>
     );
 }
