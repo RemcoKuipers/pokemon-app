41166ad9f315a201454f7ebd8d971eb38d610b0d
RemcoKuipers <remslaap@live.nl>
2026-04-09 09:15:57 +0200

    Logout button toegevoegd met een fixed position en een navigate naar login als er uitgelogd is

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route, useNavigate} from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx";
+import {useAuth} from "./context/AuthContext.jsx";
 import Home from "./pages/Home/Home";
 import Search from "./pages/Search/Search";
 import CardDetail from "./pages/CardDetail/CardDetail";
@@ -8,26 +9,45 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css"
+
 function App() {
+
+    const {logout, isAuthenticated} = useAuth();
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        logout();
+        navigate("/login");
+    }
+
     return (
         <div className="layout">
-            <Navbar />
+            <Navbar/>
+
+            {isAuthenticated && (
+                <button
+                    className="logout-button"
+                    onClick={handleLogout}
+                >
+                    Logout
+                </button>
+            )}
 
             <main className="content">
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/search" element={<Search />} />
-                    <Route path="/card/:id" element={<CardDetail />} />
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/search" element={<Search/>}/>
+                    <Route path="/card/:id" element={<CardDetail/>}/>
                     <Route
                         path="/collection"
                         element={
                             <ProtectedRoute>
-                                <Collection />
+                                <Collection/>
                             </ProtectedRoute>
                         }
                     />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
                 </Routes>
             </main>
         </div>
